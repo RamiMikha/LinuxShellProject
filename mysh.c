3ed8394757d41dea7b77579f1bc41d525d8c6c98
RamiMikha <rmikh229@mtroyal.ca>
2024-09-19 04:25:25 +0000

    Made Clear Buffer Function and Fixed Known Bugs

--- mysh.c
+++ mysh.c
@@ -8,25 +8,33 @@
 
 /*
 Things to do:
-- Need to make a buffer clear function (set all values inside the buffer to null, using the bytes_read variable as a determining way of how much is needed
 - Make our own string comparison function
 - Move the messages into its own file (.h) named smth like messages.h
 - Stop using stdio.h and string.h
 
 Known Bugs:
-- Doesn't print out the prompt upon first boot for some reason
-- Since the buffer isn't being cleared after every read, if you type out for example hello and then type t on the next prompt, output is tello
+
 */
+
+
+void clear_buffer(char *buffer, int size){
+  for (int i = 0; i < size; i++){
+    buffer[i] = '\0';
+  }
+}
 
 int main(){
     char string_buffer[256];
     char error1 [] = "Exiting Program\n";
     char command_prompt [] = "$ ";
     int bytes_read;
+
+    clear_buffer(string_buffer, 256);
+    write(1, command_prompt, 2);
     
     bytes_read = read(0,string_buffer, 256);
     string_buffer[bytes_read-1] = '\0'; /*the read funciton does not null terminate*/
-    write(1, command_prompt, 2);
+ 
     
     while (strcmp(string_buffer, "exit") != 0){
       if(strcmp(string_buffer, "exit") == 0)
@@ -36,6 +44,7 @@ int main(){
 	write(1, string_buffer, 256);
 	write(1, "\n", 1);
 
+	clear_buffer(string_buffer, 256); /*Clear the buffer*/
 	write(1, command_prompt, 2);
 	bytes_read = read(0,string_buffer,256);
 	string_buffer[bytes_read-1] = '\0';
